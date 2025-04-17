d2692299601b56191ef21b92d641bdb03afb1af8
ghgbwd <nguyenthanhthuong.tto@gmail.com>
2025-04-17 20:55:24 +0700

    lan 2

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -54,8 +54,67 @@ const getUserByIDController = async (req, res) => { //req: request : hiểu tạ
         res.status(500).json({ message: "Error from server!" })
     }
 }
+
+//cap nhat user
+
+// const updateUserController = async (req, res) => {
+//     const id = req.params.id;
+//     const { name, age, email, password, description } = req.body;
+//     try {
+//         const user = {
+//             name: name,
+//             age: age,
+//             email: email,
+//             password: password,
+//             description: description ? description : "",
+//             createdAt: new Date(),
+//             updatedAt: new Date(),
+//         }
+//         await Users.findByIdAndUpdate(id,user);
+//         res.status(201).json({
+//             message: "Update thanh cong",
+//             user
+//         })
+//     } catch (error) {
+//         console.log("Error from server!", error);
+//         res.status(500).json({ message: "error from server!" });
+//     }
+// }
+
+const updateUserController = (req, res) => {    //async dùng trong bất đồng bộbộ
+    const id = req.params.id; // gán biến id bằng req.params.id
+    
+    const { name, age, email, password, description } = req.body;  //nhập dữ liệu vào req.body
+    console.log(req.body); //hiển thị dữ liệu được khai báo trong terminal
+    
+    // const name = req.body.name;
+    try {     // try catch sử dụng để phân luồng dữ liệu, dữ liệu đi qua try ko lỗi sẽ hiển thị dữ liệu ngược lại sẽ bắt đc lỗi, tạo ra ngoại lệ giúp chương trình ko bị dừng
+        const user = {
+            name: name,
+            email: email,
+            age: age,
+            password: password,
+            description: description ? description : "",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }
+        Users.findByIdAndUpdate(id, user); 
+        res.status(201).json({
+            message: "Update thanh cong",
+            user
+        })
+        
+    } catch (error) { 
+        console.log("Error from server ! thuong code loi roi kia", );
+        res.status(500).json({message:"error from server!"});
+    }
+}
+
+
+
 module.exports = {
     createUserController,
     getAllUserController,
     getUserByIDController,
+    updateUserController,
 }
--- src/route/userRoute.js
+++ src/route/userRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { createUserController, getAllUserController, getUserByIDController } = require("../controller/userController");
+const { createUserController, getAllUserController, getUserByIDController, updateUserController } = require("../controller/userController");
 
 let router = express.Router();
 router.post("/create", createUserController)
 router.get("/users", getAllUserController)
 router.get("/user/:id", getUserByIDController)
+router.put("/user/:id",updateUserController)
 
 module.exports = router;
